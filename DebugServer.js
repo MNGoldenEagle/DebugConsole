a6ee80b182197686e3c099c80bfe872ad64706fe
MNGoldenEagle <17274702+MNGoldenEagle@users.noreply.github.com>
2020-11-23 01:12:17 -0600

    Updated DebugServer.js with startup logging

--- DebugServer.js
+++ DebugServer.js
@@ -3,20 +3,18 @@ var socket = null;
 
 debugServer.on('connection', function(newSocket) {
 	socket = newSocket;
+	console.print("Connection to client established.\n");
+	socket.write("Server reporting ready status.\n");
 	
 	newSocket.on('close', function() {
+		console.print("Connection to client lost.\n");
 		socket = null;
 	});
 });
 
 events.onexec(0x800021B0, function() {
-	var code = gpr.a0;
 	var addr = gpr.a1;
 	var len = gpr.a2;
-	
-	if (code > 1) {
-		socket.write('\n\n\n!!!Code was ' + code + ' on addr ' + addr + ' with len ' + len + '\n\n\n');
-	}
 	
 	var str = mem.getblock(addr, len);
 	
@@ -24,12 +22,3 @@ events.onexec(0x800021B0, function() {
 		socket.write(str);
 	}
 });
-
-// A0 = 0, A1 = no-var string size 30, A2 = 0x30
-// Result: should transfer entire string up to A2 - 1 (A2 would include \0)
-// A0 = 0, A1 = var string size 53, A2 = 0x45
-// Result: should transfer entire string up to A2 - 1 (A2 would include %)
-// A0 = 1, A1 = no-var string size D, A2 = 0xD
-// Result: should transfer entire string up to A2
-// A0 = 1, A1 = no-var string size 1, A2 = 1
-// Result: should transfer entire string up to A2
